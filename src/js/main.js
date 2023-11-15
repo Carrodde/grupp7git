9f5a3f12e2b413b2ce73dd6905eb4677d1082b24
Carolina Ekdal <Carolina.ekdal@gmail.com>
2023-11-15 14:51:02 +0100

    merge created list and styled

--- src/js/main.js
+++ src/js/main.js
@@ -9,14 +9,24 @@ const rebus = new CPets("Rebus", "Horse", 9, "brown");
 let petsList = [siri, sam, gazette, rebus];
 console.log(petsList);
 
-const divTag = document.createElement("div");
+const divTag = document.getElementById("app");
 document.body.appendChild(divTag);
 
 for (let i = 0; i < petsList.length; i++) {
   const container = document.createElement("div");
   const nameTitle = document.createElement("h2");
   nameTitle.innerHTML = petsList[i].name;
+  const infoSpec = document.createElement("p");
+  infoSpec.innerHTML = petsList[i].species;
+  const infoAge = document.createElement("p");
+  infoAge.innerHTML = petsList[i].age;
+  const infoColor = document.createElement("p");
+  infoColor.innerHTML = petsList[i].color;
 
   container.appendChild(nameTitle);
+  container.appendChild(infoSpec);
+  container.appendChild(infoAge);
+  container.appendChild(infoColor);
   divTag.appendChild(container);
+  container.className = "box";
 }
