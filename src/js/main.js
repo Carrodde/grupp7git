616464339a1f0e67235f090462a2e50688e6c868
Carolina Ekdal <Carolina.ekdal@gmail.com>
2023-11-15 14:34:07 +0100

    create branch, made a list

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
-import "./../css/style.css";
+import "../css/style.css";
+import { CPets } from "./models/CPets";
+
+const siri = new CPets("Siri", "Cat", 3, "brown");
+const sam = new CPets("Sam", "Cat", 3, "Black & White");
+const gazette = new CPets("Gazette", "Horse", 13, "Yellowblack");
+const rebus = new CPets("Rebus", "Horse", 9, "brown");
+
+let petsList = [siri, sam, gazette, rebus];
+console.log(petsList);
+
+const divTag = document.createElement("div");
+document.body.appendChild(divTag);
+
+for (let i = 0; i < petsList.length; i++) {
+  const container = document.createElement("div");
+  const nameTitle = document.createElement("h2");
+  nameTitle.innerHTML = petsList[i].name;
+
+  container.appendChild(nameTitle);
+  divTag.appendChild(container);
+}
