6d31e98a670d13d7f4ed40cc6548a2603ba3ff76
Thefirat <firat.akkoyun@medieinstitutet.se>
2023-11-15 14:38:29 +0100

    Books list created.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
-import "./../css/style.css";
+import "../css/book.css";
+import { createHtmlBook } from "./htmlHelpers";
+import { Book } from "./models/Book";
+
+const bok1 = new Book("Jag har testat allt", "David Haroun", 2023);
+const bok2 = new Book("Ödet och hoppet", " Niklas Natt och Dag", 2022);
+const bok3 = new Book("Lille prinsen", "Antoine de Saint-Exupéry", 1977);
+
+const books = [bok1, bok2, bok3];
+
+for (let i = 0; i < books.length; i++) {
+  createHtmlBook(books[i]);
+}
---
+++ src/js/models/Book.js
@@ -0,0 +1,12 @@
+export class Book {
+    name; 
+    author;
+    year;
+
+    constructor(name, author, year)
+    {
+        this.name = name;
+        this.author = author; 
+        this.year = year;
+    }
+}
